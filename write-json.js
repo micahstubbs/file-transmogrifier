46bc9f199130957caa7c4026f2cbd2f39a9f6f48
Micah Stubbs <micah.stubbs@gmail.com>
2018-06-22 23:59:41 -0700

    console.error

--- write-json.js
+++ write-json.js
@@ -2,6 +2,6 @@
 const jf = require('jsonfile')
 module.exports = function(data, path, filename) {
   jf.writeFile(`${path}/${filename}`, data, { spaces: 2 }, function(err) {
-    console.log(err)
+    console.error(err)
   })
 }
