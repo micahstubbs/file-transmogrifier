4345fbe36ebb70cf01c2b56bb5451f313689164c
Micah Stubbs <micah.stubbs@gmail.com>
2018-06-22 21:53:30 -0700

    add utils for writing json and csv

---
+++ write-csv.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const csvWriter = require('csv-write-stream')
+
+function writeCSV(outputFile, outputData) {
+  // write a csv file
+  const writer = csvWriter()
+  writer.pipe(fs.createWriteStream(outputFile))
+  outputData.forEach(d => {
+    writer.write(d)
+  })
+  writer.end()
+}
+
+module.exports = writeCSV
